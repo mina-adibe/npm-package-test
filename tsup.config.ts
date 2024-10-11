701523367b8b83651d912b44e4286fcf7db187ab
Mina Bessada <mina.adibe@gmail.com>
2024-10-11 04:44:35 -0400

    Refactor build setup for better compatibility and control

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', '!src/**/*.spec.*' , '!src/**/*.test.*'],
   format: ['cjs', 'esm'],
   dts: true,
   outDir: 'dist',
   clean: true,
+  platform: 'node',
+  splitting: true,
+  sourcemap: true,
+  minify: false,
+  external: ['typescript', 'vitest'],
 });
